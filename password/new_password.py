e2a1304a8b28a9c49ecb684820e3dc1fe49dc9aa
yetginer-ogun <yetginer.mehmetogun@gmail.com>
2026-02-28 21:19:30 +0300

    testler eklendi

--- password/new_password.py
+++ password/new_password.py
@@ -5,10 +5,16 @@ def generate_password(length=12):
     """Belirtilen uzunlukta rastgele bir şifre oluşturur."""
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''
+    if length < 6:
+        return "Şifre en az 6 karakter içermelidir"
     for i in range(length):
         password += random.choice(characters)
     return password
 
 # Kullanım örneği
-password_length = 12  # İstediğiniz herhangi bir şifre uzunluğunu seçebilirsiniz
+password_length = 3  # İstediğiniz herhangi bir şifre uzunluğunu seçebilirsiniz
 print("Yeni şifreniz:", generate_password(password_length))
+
+
+
+
--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -12,6 +12,22 @@ def test_password_characters():
 Aşağıda önerilenlerden birini kullanarak başka bir test yazın. Alternatif olarak, kendi testinizi de oluşturabilirsiniz!
 Daha fazla test yazabilirseniz harika olur!
 
-1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin  
+1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin   
 2. Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test edin 
 """
+
+def test_password_length():
+    password = generate_password(15)
+    assert len(password) == 15, "Şifrenini uzunluğu girilen uzunluğa eşit değil"
+
+
+def test_regenerate_password():
+    password1 = generate_password(15)
+    password2 = generate_password(15)
+    assert password1 != password2 , "Şifreler birbiriyle aynı"
+
+
+def test_minimum_length():
+    password = generate_password(3)
+    assert password != "Şifre en az 6 karakter içermelidir", "3 haneli şifre oluşturuldu"
+
