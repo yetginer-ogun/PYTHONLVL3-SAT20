74de3e88967f6c39a60aa12375fef7edcbccc7d2
Yusuf Berk Demirel <yusufdicordbrk@gmail.com>
2026-02-28 21:25:17 +0300

    as

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -15,3 +15,16 @@ Daha fazla test yazabilirseniz harika olur!
 1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin  
 2. Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test edin 
 """
+
+def test_password_length():
+    """Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test eder"""
+    length = 15
+    password = generate_password(length)
+    assert len(password) == length
+
+def test_passwords_are_different():
+    """Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test eder"""
+    password1 = generate_password(12)
+    password2 = generate_password(12)
+    assert password1 != password2
+
